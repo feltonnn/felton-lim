9624a431b44d86d0dc852b47d472ee9128374e65
felton <85930011+feltonnn@users.noreply.github.com>
2024-12-14 18:24:22 +0800

    Fix Navbar bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import Hero from "./components/Hero"
-import Navbar from "./components/NavBar"
+import Navbar from "./components/Navbar"
 import Projects from "./components/Projects"
 import Technologies from "./components/Technologies"
 import Experience from "./components/Experience"
